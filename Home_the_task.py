2575f0bf79ea7a5dcc4c79f2ff21413ecf5b04bf
Alexander <baskakoff.aleks2015@yandex.ru>
2021-04-05 21:31:21 +0300

    first lesson, getting to know Python
    I created a simple mechanism that determines the current day, whether it is my birthday. I also created a countdown that shows how many days are left until my birthday.

---
+++ Home_the_task.py
@@ -0,0 +1,13 @@
+# My birthday
+import datetime
+
+now = datetime.datetime.today()
+month = now.month
+year = now.year
+
+if now.day == 6 and now.month == 8 and now.year == 2021: #Если сегодня эта дата, то это мой день рождения
+    print("День моего рождения")
+    exit(0)
+
+else: #Если дата другая, то не мой день рождения
+    print("Не мой день рождения")
