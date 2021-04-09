65ead0865a227f6a69e2278f6b67c7c41e1772af
Alexander <baskakoff.aleks2015@yandex.ru>
2021-04-09 21:17:44 +0300

    Correction of remarks

--- Countdown_to_my_birthday.py
+++ Countdown_to_my_birthday.py
@@ -2,6 +2,6 @@
 import datetime
 
 now = datetime.datetime.today() #Cегодняшний день
-bd = datetime.datetime(2021, 8, 6) #Дата дня рождения
-d = bd - now #Для подсчёта дней нужно отнять от даты дня рождения, сегодняшнюю дату
+birthday_date = datetime.datetime(2021, 8, 6) #Дата дня рождения
+d = birthday_date - now #Для подсчёта дней нужно отнять от даты дня рождения, сегодняшнюю дату
 print('До дня рождения: {} дня/дней.'.format(d.days)) #Вывод колличества дней
