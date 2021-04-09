4e560ccd63c86ee86ff260dcca311e6c78e3f524
Alexander <baskakoff.aleks2015@yandex.ru>
2021-04-09 21:22:00 +0300

    Fixed variable

--- Countdown_to_my_birthday.py
+++ Countdown_to_my_birthday.py
@@ -3,5 +3,5 @@ import datetime
 
 now = datetime.datetime.today() #Cегодняшний день
 birthday_date = datetime.datetime(2021, 8, 6) #Дата дня рождения
-d = birthday_date - now #Для подсчёта дней нужно отнять от даты дня рождения, сегодняшнюю дату
-print('До дня рождения: {} дня/дней.'.format(d.days)) #Вывод колличества дней
+timedelta_for_birthday = birthday_date - now #Для подсчёта дней нужно отнять от даты дня рождения, сегодняшнюю дату
+print('До дня рождения: {} дня/дней.'.format(timedelta_for_birthday.days)) #Вывод колличества дней
